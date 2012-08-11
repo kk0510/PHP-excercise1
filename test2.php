9b46953a12ffe665ebcc38dba8f655e54a425fbf
kk0510 <kousuke_k0510@yahoo.co.jp>
2012-08-12 01:22:17 +0900

    php test

---
+++ test2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+</head>
+<body>
+<form action="./exe2.php" method="GET">
+<input type="text" name="name">
+<input type="submit" value="送信！">
+</form>
+<?php
+echo "Let's＼(・ω・)／".htmlspecialchars($_GET['name']);
+?>
+</body>
+</html>
