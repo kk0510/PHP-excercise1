45987d3c54f2149431eb9a25ee0af2558467a3a7
kk0510 <kousuke_k0510@yahoo.co.jp>
2012-08-13 08:13:25 +0900

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,4 +1,12 @@
+<?php
+// PHPの設定に関係なくエラーを厳密にチェックし、画面に表示するための設定
+ini_set('display_errors', 1);
+error_reporting(-1);
 
+// nameというキーが存在しないときに参照するとNoticeエラーになります。
+// 存在チェックを必ず行い、なければ初期化を行います
+$name = (isset($_GET['name']))? $_GET['name']: "";
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html lang="ja">
 <head>
@@ -11,9 +19,7 @@
 <input type="submit" value="送信！">
 </form>
 <?php
-ini_set('display_errors', 1);
-error_reporting(-1);
-echo "Let's＼(・ω・)／".htmlspecialchars($_GET['name']);
+echo "Let's＼(・ω・)／".htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
 ?>
 </body>
 </html>
